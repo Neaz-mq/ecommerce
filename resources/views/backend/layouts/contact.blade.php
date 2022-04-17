b29e99b2c869b9b31a6b3e237dc96c0b18113e7d
Neaz-mq <neazmorshed666@gmail.com>
2022-04-17 19:53:05 +0600

    2nd

---
+++ resources/views/backend/layouts/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('backend.master')
+
+@section('content')
+<h1>kkkk</h1>
+@endsection
---
+++ resources/views/backend/layouts/home.blade.php
@@ -0,0 +1,10 @@
+@extends('backend.master')
+
+@section('content')
+
+<h1>My home</h1>
+<h1>My home</h1>
+<h1>My home</h1>
+<h1>My home</h1>
+
+@endsection
