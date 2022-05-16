0fe9d612eaa607e6fbe3281b9482aa35197ed9d0
Neaz-mq <neazmorshed666@gmail.com>
2022-05-16 11:14:06 +0600

    5th

---
+++ database/migrations/2022_05_16_102120_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->double('price');
+            $table->string('name');
+            $table->integer('stock')->default('0');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status', 10)->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
